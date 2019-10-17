35f8509b080deba8f804b0fd26d29b1332422b0b
smboy86 <smboy86@naver.com>
2019-10-18 01:43:18 +0900

    #10 App: Setup ~ (1)

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { AppLoading } from "expo";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import { Text, View, AsyncStorage } from "react-native";
+
+// import AsyncStorage from "@react-native-community/async-storage";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { persistCache } from "apollo-cache-persist";
+import ApolloClient from "apollo-boost";
+import { ThemeProvider } from "styled-components";
+import apolloClientOptions from "./apollo";
+import { ApolloProvider } from "react-apollo-hooks";
+import styles from "./styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+  const [loaded, setLoaded] = useState(false);
+  const [client, setClient] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+  const preLoad = async () => {
+    await Font.loadAsync({
+      ...Ionicons.font
+    });
+    await Asset.loadAsync([require("./assets/logo.png")]);
+
+    const cache = new InMemoryCache();
+
+    await persistCache({
+      cache,
+      storage: AsyncStorage
+    });
+
+    const client = new ApolloClient({
+      cache,
+      ...apolloClientOptions
+    });
+    const isLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+    if (isLoggedIn === null || isLoggedIn === false) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+    setLoaded(true);
+    setClient(client);
+  };
+
+  useEffect(() => {
+    preLoad();
+  }, []);
+
+  return loaded && client && isLoggedIn !== null ? (
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={styles}>
+        <View>
+          {isLoggedIn === true ? <Text>I'm in </Text> : <Text>I'm out</Text>}
+        </View>
+      </ThemeProvider>
+    </ApolloProvider>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # 프리스마그램 앱 스터디
+
+yarn add styled-components react-navigation apollo-boost graphql react-apollo-hooks
+
+yarn add @expo/vector-icons
+
+yarn add expo-font
+
+yarn add expo-asset
+
+yarn add apollo-cache-persist apollo-cache-inmemory
+
+## apollo cache persist
